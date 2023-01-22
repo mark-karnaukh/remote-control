ad096920749840eb947a6d79f2d99c7099e3c34f
Mark Karnaukh <marik13021992@gmail.com>
2023-01-22 17:09:16 +0100

    Add .env variables

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
+import * as dotenv from 'dotenv';
 import { httpServer } from './src/http_server/index.js';
-import { mouse } from "@nut-tree/nut-js";
+// import { mouse } from "@nut-tree/nut-js";
 
-const HTTP_PORT = 8181;
+dotenv.config();
+
+const HTTP_PORT = process.env.HTTP_PORT || 8181;
+
+console.log('HTTP_PORT: ', process.env.HTTP_PORT);
+console.log('WS_PORT: ', process.env.WS_PORT);
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+process.on('SIGINT', function () {
+  console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
+  // some other closing procedures go here
+  process.exit(0);
+});
